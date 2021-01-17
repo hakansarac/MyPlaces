70dd9baffa28c5aafd3d9f415774a3d49d194278
hakansarac <hakan.sarac95@gmail.com>
2021-01-17 21:36:15 +0300

    kotlin-android-extensions added and AddNewPlaceActivity UI created

--- app/src/main/java/com/hakansarac/myplaces/AddNewPlaceActivity.kt
+++ app/src/main/java/com/hakansarac/myplaces/AddNewPlaceActivity.kt
@@ -1,11 +1,22 @@
 package com.hakansarac.myplaces
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_add_new_place.*
 
 class AddNewPlaceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_new_place)
+
+
+        setSupportActionBar(toolbarAddNewPlace)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)   //add back button
+        toolbarAddNewPlace.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
+        buttonSave.setBackgroundColor(Color.parseColor("#FFFFBA93"))
     }
 }
--- app/src/main/java/com/hakansarac/myplaces/MainActivity.kt
+++ app/src/main/java/com/hakansarac/myplaces/MainActivity.kt
@@ -4,15 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    fun buttonAddNewPlace(view:View){
-        val intent = Intent(this,AddNewPlaceActivity::class.java)
-        startActivity(intent)
+        fabAddNewPlace.setOnClickListener {
+            val intent = Intent(this,AddNewPlaceActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
