adc85b836482a8e23eae7a97abaabed1912a64a7
hakansarac <hakan.sarac95@gmail.com>
2021-01-17 22:09:51 +0300

    DatePickerDialog added to select date from calendar

--- app/src/main/java/com/hakansarac/myplaces/AddNewPlaceActivity.kt
+++ app/src/main/java/com/hakansarac/myplaces/AddNewPlaceActivity.kt
@@ -1,14 +1,23 @@
 package com.hakansarac.myplaces
 
+import android.app.DatePickerDialog
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_add_new_place.*
+import java.text.SimpleDateFormat
+import java.util.*
 
-class AddNewPlaceActivity : AppCompatActivity() {
+class AddNewPlaceActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var calendar = Calendar.getInstance()
+    private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_new_place)
+
 
 
         setSupportActionBar(toolbarAddNewPlace)
@@ -18,5 +27,27 @@ class AddNewPlaceActivity : AppCompatActivity() {
         }
 
         buttonSave.setBackgroundColor(Color.parseColor("#FFFFBA93"))
+
+        dateSetListener = DatePickerDialog.OnDateSetListener{ view, year,month, dayOfMonth ->
+            calendar.set(Calendar.YEAR,year)
+            calendar.set(Calendar.MONTH,month)
+            calendar.set(Calendar.DAY_OF_MONTH,dayOfMonth)
+            updateDateView()
+        }
+        editTextDate.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        when(p0!!.id){
+            R.id.editTextDate -> {
+                DatePickerDialog(this@AddNewPlaceActivity, dateSetListener,calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH)).show()
+            }
+        }
+    }
+
+    private fun updateDateView(){
+        val calendarFormat = "dd.MM.yyyy"
+        val sdf = SimpleDateFormat(calendarFormat,Locale.getDefault())
+        editTextDate.setText(sdf.format(calendar.time).toString())
     }
 }
