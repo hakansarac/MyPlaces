3ea48b339f3a7fc3bab0b24c249d24c4d95f9c20
hakansarac <hakan.sarac95@gmail.com>
2021-01-17 17:47:03 +0300

    AddNewPlace activity created

---
+++ app/src/main/java/com/hakansarac/myplaces/AddNewPlaceActivity.kt
@@ -0,0 +1,11 @@
+package com.hakansarac.myplaces
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddNewPlaceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_new_place)
+    }
+}
